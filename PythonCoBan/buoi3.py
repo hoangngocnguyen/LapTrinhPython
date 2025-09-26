27afcd622ceecdace0bf7bdcaefe34b96a29d60e
hoangngocnguyen <nguyenhoang19112005@gmail.con>
2025-09-26 18:09:12 +0700

    Add: Tim cac so nguyen to nho hon 200

--- PythonCoBan/buoi3.py
+++ PythonCoBan/buoi3.py
@@ -61,13 +61,19 @@
 n = int(input("nhap n: "))
 
 
-def isSoNguyenTo(n):
+def is_so_nguyen_to(n):
     if n < 2:
         return False
-    for i in range(2, int(n / 2) + 1):
+    for i in range(2, int(n**(1/2)) + 1):
         if n % i == 0:
             return False
     return True
 
 
-print(f"{n} la so nguyen to? {isSoNguyenTo(n)}")
+print(f"{n} la so nguyen to? {is_so_nguyen_to(n)}")
+
+print(f"Các số nguyên tố không lớn hơn 200: ")
+for i in range(2, 201):
+    if is_so_nguyen_to(i):
+        print(i, end = " ")
+print()
